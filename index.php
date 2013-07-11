f57499b146bc89c2b760d60f41671d8ee4f42028
wouerner <wouerner@gmail.com>
2013-07-11 19:33:16 -0300

    commit init

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include ('head/head.html'); ?>
+
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="span12">
+            <?php include ('nav/menu/topMenu.html');?>
+        </div>
+    </div>
+    <?php include ('nav/menu/navBar.html');?>
+    <?php include ('carousel/thumbnailCarousel.html');?>
+    <?php include ('blog/blog.html');?>
+    <?php include ('footer/footer.html');?>
+</div>
